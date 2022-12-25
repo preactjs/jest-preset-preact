b93b29b4786d76d22335336bfccce0b3f31e9f2c
Kent Richards <info@kentrichards.net>
2022-12-24 19:02:36 -0700

    Use babel config file if present

--- src/babel.js
+++ src/babel.js
@@ -1,26 +1,36 @@
 const babelJest = require('babel-jest');
+const babel = require('@babel/core');
 
-module.exports = babelJest.default.createTransformer({
-	presets: [
-		[
-			'@babel/preset-typescript',
-			{
-				jsxPragma: 'h',
-				jsxPragmaFrag: 'Fragment',
-			},
+// Use config file if present.
+const config = babel.loadPartialConfig();
+const options = config.files.size
+	? config.options
+	: {
+		presets: [
+			[
+				'@babel/preset-typescript',
+				{
+					jsxPragma: 'h',
+					jsxPragmaFrag: 'Fragment',
+				},
+			],
+			'@babel/preset-env',
 		],
-		'@babel/preset-env',
-	],
-	plugins: [
-		[
-			'@babel/plugin-transform-react-jsx',
-			{
-				pragma: 'h',
-				pragmaFrag: 'Fragment',
-			},
+		plugins: [
+			[
+				'@babel/plugin-transform-react-jsx',
+				{
+					pragma: 'h',
+					pragmaFrag: 'Fragment',
+				},
+			],
+			'@babel/plugin-proposal-class-properties',
 		],
-		'@babel/plugin-proposal-class-properties',
-	],
-	babelrc: false,
-	configFile: false,
-});
+		babelrc: false,
+		configFile: false,
+	};
+
+module.exports = {
+	...babelJest.default,
+	createTransformer: () => babelJest.default.createTransformer(options),
+};
