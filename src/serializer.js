ea948a55e61a35a94bc9b4472c33a37f5a6751bb
Mathias Schaefer (molily) <molily@mailbox.org>
2022-12-29 10:29:59 +0100

    Restore old import name

--- src/serializer.js
+++ src/serializer.js
@@ -1,4 +1,4 @@
-const render = require('preact-render-to-string/jsx');
+const renderToString = require('preact-render-to-string/jsx');
 
 module.exports = {
 	test(value) {
@@ -7,7 +7,7 @@ module.exports = {
 		);
 	},
 	print(value, serialize, indent) {
-		const result = render(value);
+		const result = renderToString(value);
 		return indent(result);
 	},
 };
